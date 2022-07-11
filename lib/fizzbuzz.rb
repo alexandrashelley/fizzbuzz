430787a582292eb601d5eb15bd93c49031266d90
naomischlosser <naomi_schlosser@hotmail.com>
2022-07-11 15:50:05 +0100

    Final changes

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,2 +1,13 @@
 def fizzbuzz(number)
+  if  number % 3 == 0 && number % 5 == 0
+    return 'fizzbuzz'
+  elsif number % 3 == 0
+    return 'fizz'
+  elsif number % 5 == 0
+    return 'buzz'
+  else
+    return number
+  end
 end
+
+(1..100).each {|number| puts "#{number} --> #{fizzbuzz(number)}"}
